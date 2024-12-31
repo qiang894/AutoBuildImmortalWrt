852afcb147ddbd20abf3c4cf20c630cedd4cfb3a
qiang894 <127944343+qiang894@users.noreply.github.com>
2024-12-31 10:50:32 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -28,7 +28,6 @@ PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn"
 PACKAGES="$PACKAGES ddns-scripts-cloudflare"
-PACKAGES="$PACKAGES luci-app-ssr"
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
 echo "$PACKAGES"
